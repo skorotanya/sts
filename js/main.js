07eb04be53e58ed6f3d9d6dd9a189bef4f439c01
skorotanya <skorotanya@gmail.com>
2023-01-19 20:12:58 +0300

    Add Excel button, add preloader, add manual show popup.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,76 @@
 });*/
 
 
+/*  PRELOADER */
+
+// hide preloader
+const hidePreloader = () => {
+    const pl = document.querySelector('.preloader');
+    pl.classList.add('preloader__hiding');
+    setTimeout(function(){
+        pl.classList.add('preloader__hide');
+        pl.classList.remove('preloader__hiding');
+    }, 500);
+}
+
+
+// show preloader
+const showPreloader = () => {
+    const pl = document.querySelector('.preloader');
+    pl.classList.remove('preloader__hide');
+    pl.classList.add('preloader__showing');
+    setTimeout(function(){
+        pl.classList.remove('preloader__showing');
+    }, 100);
+}
+
+// preloader on load page
+window.onload = () => {
+    hidePreloader();
+};
+
+
+/* POPUP (MODAL) WINDOW */
+
+// show modal
+// parameter: modalName (popup element id)
+const showModal = (modalName) => {
+    let modalDiv = document.querySelector('#' + modalName);
+    let modal = bootstrap.Modal.getOrCreateInstance(modalDiv);
+    modal.show();
+}
+
+//hide modal
+// parameter: modalName (popup element id)
+const hideModal = (modalName) => {
+    let modalDiv = document.querySelector('#' + modalName);
+    let modal = bootstrap.Modal.getOrCreateInstance(modalDiv);
+    modal.hide();
+}
+
+
+// show popup information about user with preloader
+document.querySelector('.user-name').addEventListener('click', (e) => { 
+    showPreloader();
+    setTimeout(function(){
+        hidePreloader();
+        showModal('infoUser');
+    }, 500);
+});
+
+
+// show popup on user exit
+document.querySelector('.user__exit').addEventListener('click', (e) => {
+    showModal('exitUser');
+});
+
+
+
+
 $(document).ready(function () {
+
+
+
     let table1 = $('#tso_table').DataTable({
     	language: {
             url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/ru.json'
@@ -30,34 +99,6 @@ $(document).ready(function () {
             	config: {
                     depthLimit: 1
                 }
-            },
-            {
-            	extend: 'collection',
-            	text: '...',
-            	className: 'bttn bttn-color',
-            	buttons: [
-            		{
-            			extend: 'selectedSingle',
-            			text: 'Удалить',
-            			action: function ( e, dt, node, config ) {
-		                    alert( 'Удалить строку' );
-		                }
-
-            		},
-            		{
-            			extend: 'selectedSingle',
-            			text: 'Редактировать',
-            			action: function ( e, dt, node, config ) {
-		                    alert( 'Редактировать строку' );
-		                }
-            		},
-            		{
-            			text: 'Добавить',
-            			action: function ( e, dt, node, config ) {
-		                    alert( 'Добавить строку' );
-		                }
-            		}
-            	]
             }
         ],
 
@@ -65,25 +106,15 @@ $(document).ready(function () {
         
     });
 
-    $('#tso2_table').DataTable({
-    	language: {
-            url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/ru.json'
-        },
-        scrollY: '500px',
-        scrollX: true,
-        scrollCollapse: true,
-        paging: false,
-        info: false,
-        buttons: [
-            'colvis','searchBuilder'
-        ],
-        dom: 'Brt'
-        
-
-    });
-
+    //обработка события на двойной клик по строке
     $('#tso_table tbody').on('dblclick', 'tr', function () {
         let data1 = table1.row(this).data();
         alert('You clicked on ' + data1[0] + " row");
     });
+
+
+    // привязка события выгрузки данных таблицы в excel на кнопку с классом bttn-excel-export
+    $('.bttn-excel-export').click(() => {
+        $('#tso_table').DataTable().buttons('.buttons-excelHtml5').trigger();
+    })
 });
