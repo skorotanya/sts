7a47ab4f0b0b020e6fc1ca900255a0b564414427
skorotanya <skorotanya@gmail.com>
2023-02-26 18:54:47 +0300

    Add tooltip at table, poiter cursor at clickable table, new style disabled inputs.

--- js/main.js
+++ js/main.js
@@ -237,10 +237,6 @@ document.querySelector('.user__exit').addEventListener('click', (e) => {
     showModal('exitUser');
 });
 
-
-
-
-
-
-
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
