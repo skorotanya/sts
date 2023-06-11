b7d81957112316a22a8a38a0301f0483f6f46e9b
skorotanya <skorotanya@gmail.com>
2023-06-11 11:43:23 +0400

    Add pages for views 2.6.10, 2.6.13, 2.6.15, 2.6.16, 2.6.17, 2.6.18.

---
+++ js/page2-6-13.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+
+    // javascript functions for current page
+
+
+
+     // init dataTables on current page
+
+  /*  const tsoCols = [
+    { "width": "5%" },
+    { "width": "10%" },
+    { "width": "15%" },
+    { "width": "10%" },
+   { "width": "10%" },
+   { "width": "10%" },
+   { "width": "10%" },
+   { "width": "10%" },
+   { "width": "10%" },
+   { "width": "10%" }
+  ];*/
+
+    const pageTables = [
+    {
+      name: 'refModelLayers',
+      type: 'full',
+      height: '73vh',
+      columns: [],
+      modal: '',
+      tab: false,
+      firstTab: false
+    }
+  ];
+
+  initPageTables(pageTables);
+ 
+
+    // event listeners on current page
+
+     // show modal window for edit data
+    $('.bttn-edit-data').on('click', (e) => {
+        showModal('editData');
+    });
+
+    /* Double click on row ecoTable */
+    $('#refModelLayersTable tbody').on('dblclick', 'tr', function () {
+      showModal('editData');
+    } );
+
+    $('#radioLayerDelete').change(function(){
+      let inp = document.querySelector('#inpYearDelete');
+      inp.value = '';
+      inp.disabled = !this.checked;
+      inp= document.querySelector('#inpCauseDelete');
+      inp.value = '';
+      inp.disabled = !this.checked;
+    } );
+
+});
+
